5b54e2249ccc1fdfb05209fbec47e0d4a1e2fe4b
LucasFerruchi <lucasferruchi05@gmail.com>
2024-06-25 08:40:55 -0300

    creo pages y paginas

---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2>LogInScreen</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LogIn;
