238d228a0040ad163da03dd4b90c246fc5f433fe
LucasFerruchi <lucasferruchi05@gmail.com>
2024-07-02 09:56:27 -0300

    desarrollo de tabla en el HOME.

---
+++ src/helpers/coinsApi.js
@@ -0,0 +1,11 @@
+const url = "https://api.coincap.io/v2/assets";
+
+let limite = 10;
+
+export const getCoins = async () => {
+  //   const coins = await fetch(url + "?limit=" + limite);
+  const coins = await fetch(`${url}?limit=${limite}`);
+
+  const data = await coins.json();
+  return data;
+};
